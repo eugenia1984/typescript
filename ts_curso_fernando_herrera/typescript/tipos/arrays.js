a6b662ccafbd2d1e2da44a2e2595acd1b5714d1e
Maria Eugenia Costa <costamariaeuenia1@gmail.com>
2023-10-26 11:23:37 -0300

    -feat: arrays

--- ts_curso_fernando_herrera/typescript/tipos/arrays.js
+++ ts_curso_fernando_herrera/typescript/tipos/arrays.js
@@ -1,4 +1,8 @@
 "use strict";
-(function () { 
-
+(function () {
+  var numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  numbers.push(11);
+  console.log(numbers);
+  var villians = ['Omega Rojo', 'Dormammu', 'Duende Verde'];
+  villians.forEach(function (villan) { return console.log(villan.toLowerCase()); });
 })()
--- ts_curso_fernando_herrera/typescript/tipos/arrays.ts
+++ ts_curso_fernando_herrera/typescript/tipos/arrays.ts
@@ -1,3 +1,12 @@
 (() => { 
-  
+  // const numbers: ( string| number | boolean)[] = [1, 2, 3, 4, 5, '6', 7, 8, 9, 10];
+  const numbers: number[] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  numbers.push(11);
+
+  console.log(numbers);
+
+  const villians = ['Omega Rojo', 'Dormammu', 'Duende Verde'];
+
+  villians.forEach( villan => console.log(villan.toLowerCase()))
+
 })()
