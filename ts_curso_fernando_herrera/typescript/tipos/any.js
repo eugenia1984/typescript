730479817cfe7fc31cce8542cddff752fec19042
Maria Eugenia Costa <costamariaeuenia1@gmail.com>
2023-10-26 10:53:22 -0300

    -agrego any

---
+++ ts_curso_fernando_herrera/typescript/tipos/any.js
@@ -0,0 +1,13 @@
+"use strict"
+  (function () {
+    var avenger = 123;
+    var exists;
+    var power;
+    avenger = 'Dr Strange';
+    // console.log(avenger.chart(0));
+    console.log(avenger.chartAt(0));
+    avenger = 150.23256415;
+    console.log(avenger.toFixed(2));
+    console.log(exists);
+    console.log(power);
+  })();
