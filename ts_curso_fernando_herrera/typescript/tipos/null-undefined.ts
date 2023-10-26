65e96ed2b7cf3e37066a70bca825f4e3afb9a2d1
Maria Eugenia Costa <costamariaeuenia1@gmail.com>
2023-10-26 13:10:23 -0300

    -ejercicio de practica

---
+++ ts_curso_fernando_herrera/typescript/tipos/null-undefined.ts
@@ -0,0 +1,10 @@
+(() => {
+  /* strickNUllChecks */
+  let nada: undefined = undefined;
+  /* valores booleanos:
+  true, false, undefined, null
+  */
+  /* null !== undefiend */
+
+  console.log(nada);
+})()
