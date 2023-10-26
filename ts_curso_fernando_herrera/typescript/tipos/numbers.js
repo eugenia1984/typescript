d034c28f165e22cdd3774b82868fd456aefccbfe
Maria Eugenia Costa <costamariaeuenia1@gmail.com>
2023-10-26 10:21:44 -0300

    -tipode datos: number

--- ts_curso_fernando_herrera/typescript/tipos/numbers.js
+++ ts_curso_fernando_herrera/typescript/tipos/numbers.js
@@ -1,4 +1,11 @@
 "use strict"
   (function () {
-    
+    var avengers = 10;
+    console.log(avengers);
+    const villans = 20;
+    if (avengers < villans) {
+      console.log('Extamso en problemas');
+    } else { 
+      console.log('Estamos salvados');
+    }
   })();
--- ts_curso_fernando_herrera/typescript/tipos/numers.ts
+++ ts_curso_fernando_herrera/typescript/tipos/numers.ts
@@ -1,3 +1,15 @@
 (() => {
-  
+  let avengers: number = 10;
+  console.log(avengers)
+
+  const villans = 20;
+
+  if (avengers < villans) {
+    console.log('Extamso en problemas')
+  } else { 
+    console.log('Estamos salvados')
+  }
+
+  // NaN es considerado un Number en JavaScript
+  // avengers = Number('55A')
  })()
