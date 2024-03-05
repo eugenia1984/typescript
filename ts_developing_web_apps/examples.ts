afa29ae8237ff0406471533814f3a3e01147a2e8
Maria Eugenia Costa <costamariaeugenia1@gmail.com>
2024-03-05 19:01:38 -0300

    feat: function with rest parameter

--- ts_developing_web_apps/examples.ts
+++ ts_developing_web_apps/examples.ts
@@ -5,21 +5,21 @@ let tupla: [string, number] = ["hi", 8, "how", 5];
 enum Color {
   Red,
   Green,
-  Blue
+  Blue,
 }
 
 let blue = Color.Blue;
 
 // Interface
 interface Calc {
-  subtract (first: number, second: number) : number
+  subtract(first: number, second: number): number;
 }
 
 let Calculator: Calc = {
-  subtract (first: number, second: number) : number {
+  subtract(first: number, second: number): number {
     return first - second;
-  }
-}
+  },
+};
 
 // Function
 function add(number1: number, number2: number): number {
@@ -27,9 +27,9 @@ function add(number1: number, number2: number): number {
 }
 
 // Anonymous function
-let sum = function(numberX: number, numberY: number): number {
+let sum = function (numberX: number, numberY: number): number {
   return numberX + numberY;
-}
+};
 
 // Generic
 function identity<T>(arg: T): T {
@@ -43,9 +43,13 @@ let output2 = identity<number>(100);
 function f() {
   console.log("f(): evaluated");
 
-  return function (target, propertyKey: string, descriptor: PropertyDescriptor) {
+  return function (
+    target,
+    propertyKey: string,
+    descriptor: PropertyDescriptor
+  ) {
     console.log("f(): called");
-  }
+  };
 }
 
 class C {
@@ -53,26 +57,40 @@ class C {
   method() {}
 }
 
-
 // Type assertion
 let otherVar: any = "hello";
 let myVar: any = 12;
 myVar = otherVar as string;
-console.log(typeof(myVar));
+console.log(typeof myVar);
 
 // type assertion using <>
 myVar = <string>otherVar;
-console.log(typeof(myVar));
+console.log(typeof myVar);
 
-// Function: optional parameter
-function showDetails(id: number, name: string, e_mail_id?: string) {
-
+// Function: optional and default parameter
+// ? optional
+// = 1 default
+function showDetails(id: number = 1, name: string, e_mail_id?: string) {
   console.log("ID: ", id, "Name: ", name);
 
-  if(e_mail_id != undefined) {
+  if (e_mail_id != undefined) {
     console.log("E-Mail id: ", e_mail_id);
   }
 }
 
 showDetails(101, "Andrew");
 showDetails(105, "Mary", "mary12@email.com");
+
+// Function: rest parameter
+function suma(a: number, ...b: number[]): number {
+  let result = a;
+
+  for (let i = 0; i < b.length; i++) {
+    result += b[1];
+  }
+
+  return result;
+}
+
+let result1 = suma(5, 7);
+let result2 = suma(5, 7, 9, 2);
