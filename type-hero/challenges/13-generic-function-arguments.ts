e0b7735019772fac4d9e68b9729b96a7de97d9b5
Maria Eugenia Costa <costamariaeugenia1@gmail.com>
2024-04-22 12:27:41 -0300

    Create 13-generic-function-arguments.ts

---
+++ type-hero/challenges/13-generic-function-arguments.ts
@@ -0,0 +1,28 @@
+const identity = <T,>(val: T) => val;
+
+const mapArray = <T,>(arr: T[], fn: (val: T) => T) => arr.map(fn);
+
+const expect = <T>(a: T) => ({
+  toEqual: (b: T) => a === b
+});
+
+const identity_string = identity("this is a string");
+expect(identity_string).toEqual("this is a string");
+type test_identity_string = Expect<Equal<
+  typeof identity_string,
+  "this is a string"
+>>;
+
+const identity_number = identity(123.45);
+expect(identity_number).toEqual(123.45);
+type test_identity_number = Expect<Equal<
+  typeof identity_number,
+  123.45
+>>;
+
+const identity_boolean = identity(false);
+expect(identity_boolean).toEqual(false);
+type test_identity_boolean = Expect<Equal<
+  typeof identity_boolean,
+  false
+>>;
