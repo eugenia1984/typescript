e0b7bd4ae31ceeb0253ac4773d8bb172cbabac24
eugenia1984 <costamariaeugenia1@gmail.com>
2024-12-03 11:07:32 +0100

    feat: dd completeOrder methods and put some console.logs to test

--- typescript_scrimba/index.js
+++ typescript_scrimba/index.js
@@ -6,6 +6,7 @@ const menu = [
 ]
 
 const cashInRegister = 100
+const nextOrderId = 1
 const orderQueue = []
 
 
@@ -23,14 +24,43 @@ const addNewPizza = (pizzaObj) => {
  * 2. adds the income to the cashInRegister,
  * 3. pushes a new "order object" to the orderQueue 
  *    (e.g. { pizza: selectedPizzaObjectFromStep1, status: "ordered" })
+ * 4. add the id
  * @param { String } pizzaName 
  * @returns - the new order object
  */
 const placeOrder = (pizzaName) => {
   const selectedPizza = menu.find(pizzaObj => pizzaObj.name === pizzaName)
   cashInRegister += selectedPizza.price
-  const newOrder = { pizza: selectedPizza, status: "ordered" }
+  const newOrder = { id: nextOrderId++, pizza: selectedPizza, status: "ordered" }
   orderQueue.push(newOrder)
 
   return newOrder
 }
+
+/**
+ * Finds the correct order in the orderQueue, and marks its status as "completed".
+ * @param { Number } orderId 
+ * @returns - the found order from the function
+ */
+const  completeOrder = ( orderId ) =>   {
+  const order = orderQueue.find(order => order.id === orderId)
+  order.status = "completed"
+
+  return order
+}
+
+// Add pizzas
+addNewPizza({ name: "Chicken Bacon Ranch", cost: 12 })
+addNewPizza({ name: "BBQ Chicken", cost: 12 })
+addNewPizza({ name: "Spicy Sausage", cost: 11 })
+
+// Create an order
+placeOrder("Chicken Bacon Ranch")
+
+completeOrder("1")
+
+
+// See on console
+console.log("Menu:", menu)
+console.log("Cash in register:", cashInRegister)
+console.log("Order queue:", orderQueue)
