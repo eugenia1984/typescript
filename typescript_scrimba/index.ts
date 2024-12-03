53dae699e7e11e7a2905dbf431200635d8324caa
eugenia1984 <costamariaeugenia1@gmail.com>
2024-12-03 11:59:00 +0100

    feat: add types

--- typescript_scrimba/index.ts
+++ typescript_scrimba/index.ts
@@ -1,19 +1,30 @@
-const menu = [
+type Pizza = {
+  name: string
+  price: number
+}
+
+type Order = {
+  id: number
+  pizza: Pizza
+  status: string
+}
+
+const menu: Pizza[] = [
   { name: "Margherita", price: 8 },
   { name: "Pepperoni", price: 10 },
   { name: "Hawaiian", price: 10 },
   { name: "Veggie", price: 9 },
 ]
 
-const cashInRegister = 100
-const nextOrderId = 1
-const orderQueue = []
+let cashInRegister = 100
+let nextOrderId = 1
+const orderQueue: Order[] = []
 
 /**
  * A utility function "addNewPizza" that takes a pizza object and adds it to the menu.
- * @param {Object} pizzaObj  - { name: String, price: Number}
+ * @param {Object} pizzaObj  - { name: string, price: number}
  */
-const addNewPizza = (pizzaObj) => {
+const addNewPizza = ( pizzaObj: Pizza ) => {
   menu.push(pizzaObj)
 }
 
@@ -27,8 +38,14 @@ const addNewPizza = (pizzaObj) => {
  * @param { String } pizzaName 
  * @returns - the new order object
  */
-const placeOrder = (pizzaName) => {
+const placeOrder = ( pizzaName: string ) => {
   const selectedPizza = menu.find(pizzaObj => pizzaObj.name === pizzaName)
+
+  if( !selectedPizza) {
+    console.error(`${pizzaName} does not exist in the menu`)
+    return
+  }
+
   cashInRegister += selectedPizza.price
   const newOrder = { id: nextOrderId++, pizza: selectedPizza, status: "ordered" }
   orderQueue.push(newOrder)
@@ -38,25 +55,31 @@ const placeOrder = (pizzaName) => {
 
 /**
  * Finds the correct order in the orderQueue, and marks its status as "completed".
- * @param { Number } orderId 
+ * @param { number } orderId 
  * @returns - the found order from the function
  */
-const  completeOrder = ( orderId ) =>   {
+const  completeOrder = ( orderId: number ) =>   {
   const order = orderQueue.find(order => order.id === orderId)
-  order.status = "completed"
+
+  if(!order) {
+    console.error(`${orderId} was not found in the orderQueue`)
+    return
+  }
+  
+  order.status = "completed" 
 
   return order
 }
 
 // Add pizzas
-addNewPizza({ name: "Chicken Bacon Ranch", cost: 12 })
-addNewPizza({ name: "BBQ Chicken", cost: 12 })
-addNewPizza({ name: "Spicy Sausage", cost: 11 })
+addNewPizza({ name: "Chicken Bacon Ranch", price: 12 })
+addNewPizza({ name: "BBQ Chicken", price: 12 })
+addNewPizza({ name: "Spicy Sausage", price: 11 })
 
 // Create an order
 placeOrder("Chicken Bacon Ranch")
 
-completeOrder("1")
+completeOrder(1)
 
 
 // See on console
---
+++ typescript_scrimba/variables.ts
@@ -0,0 +1,60 @@
+// Primitive data: string, number, boolean
+let myName: string = "Maria Eugenia"
+
+let numberOfWheels: number = 4
+
+let isStudent: boolean = false
+
+// custom types
+type Food = string
+let favoriteFood: Food = "pizza"
+
+// Custom type for objects
+// Entre cada key se separa con coma, con punto y como o sin nada
+type Address = {
+  street: string
+  city: string
+  country: string
+}
+
+type Person = {
+  name: string
+  age: number
+  isStudent: boolean
+  address?: Address // ? is for optional properties
+}
+
+let person1: Person = {
+  name: "Joe",
+  age: 42,
+  isStudent: true,
+  address: {
+    street: "123 Main",
+    city: "AnyTown",
+    country: "Spain"
+  }
+}
+
+let person2: Person = {
+  name: "Jill",
+  age: 66,
+  isStudent: false
+}
+
+function displayInfo( person: Person ) {
+  // As address is optional person.address?.street can be undefined
+  console.log(`${person.name} lives at ${person.address?.street || "no where"}`)
+}
+
+displayInfo(person1)
+displayInfo(person2)
+
+// Array
+let ages: number[] = [12, 46]
+ages.push(50)
+// ages.push(true) -> error
+// ages.push("50") -> error
+
+let people: Person[] = [ person1, person2 ]
+// With Generics
+let people2: Array<Person> = [ person1, person2 ]
