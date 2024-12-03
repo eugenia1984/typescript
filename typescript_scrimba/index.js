fe3b476737f596baba763446858b80aa310214e8
eugenia1984 <costamariaeugenia1@gmail.com>
2024-12-03 10:28:49 +0100

    feat: add addNewPIzza and placeOrder methods

--- typescript_scrimba/index.js
+++ typescript_scrimba/index.js
@@ -8,8 +8,29 @@ const menu = [
 const cashInRegister = 100
 const orderQueue = []
 
-/**
-* Challenge: Add a utility function "addNewPizza" that takes a pizza object
-* and adds it to the menu.
-*/
 
+/**
+ * A utility function "addNewPizza" that takes a pizza object and adds it to the menu.
+ * @param {Object} pizzaObj  - { name: String, price: Number}
+ */
+const addNewPizza = (pizzaObj) => {
+  menu.push(pizzaObj)
+}
+
+/**
+ * Takes a pizza name parameter and:
+ * 1. finds that pizza object in the menu,
+ * 2. adds the income to the cashInRegister,
+ * 3. pushes a new "order object" to the orderQueue 
+ *    (e.g. { pizza: selectedPizzaObjectFromStep1, status: "ordered" })
+ * @param { String } pizzaName 
+ * @returns - the new order object
+ */
+const placeOrder = (pizzaName) => {
+  const selectedPizza = menu.find(pizzaObj => pizzaObj.name === pizzaName)
+  cashInRegister += selectedPizza.price
+  const newOrder = { pizza: selectedPizza, status: "ordered" }
+  orderQueue.push(newOrder)
+
+  return newOrder
+}
