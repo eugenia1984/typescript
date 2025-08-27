fb210c90728e3fd49e7f6ec570f8fd75316fdf92
eugenia1984 <costamariaeugenia1@gmail.com>
2025-08-27 12:05:59 +0200

    feat: add example for Partial and Required

--- ts_codigo_facilito/advanced/utilityTypes.ts
+++ ts_codigo_facilito/advanced/utilityTypes.ts
@@ -1,2 +1,30 @@
 /****** Utility Types ******/
 // https://www.typescriptlang.org/docs/handbook/utility-types.html
+
+// Partial<T> -> devuelve la misma estructura, pero con todos los campos opcionales
+interface Task {
+  id: number;
+  name: string;
+  description: string;
+}
+
+function updateTask(id: string, task: Partial<Task>) {
+  console.log();
+}
+
+const newTask = {name: 'Maria'};
+
+updateTask('1', newTask);
+
+// Required<T> -> devuelve la misma estructura, pero con todos los campos obligatorios(required)
+interface ToDo {
+  id?: number;
+  name?: string;
+}
+
+const myToDo: Required<ToDo> = {
+  id: 0,
+  name: "Read"
+};
+
+// Records<Keys, Types> -> para la creacion de Objects
