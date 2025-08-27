fd272cca6a1ce977169e44a2f37068ad66ca968f
eugenia1984 <costamariaeugenia1@gmail.com>
2025-08-27 12:15:08 +0200

    feat: add Records and Pick

--- ts_codigo_facilito/advanced/utilityTypes.ts
+++ ts_codigo_facilito/advanced/utilityTypes.ts
@@ -1,7 +1,7 @@
 /****** Utility Types ******/
 // https://www.typescriptlang.org/docs/handbook/utility-types.html
 
-// Partial<T> -> devuelve la misma estructura, pero con todos los campos opcionales
+// Partial<T> -> return the same structure, but with all the fields optional
 interface Task {
   id: number;
   name: string;
@@ -12,11 +12,11 @@ function updateTask(id: string, task: Partial<Task>) {
   console.log();
 }
 
-const newTask = {name: 'Maria'};
+const newTask = { name: "Maria" };
 
-updateTask('1', newTask);
+updateTask("1", newTask);
 
-// Required<T> -> devuelve la misma estructura, pero con todos los campos obligatorios(required)
+// Required<T> -> return the same structure, but with all the fields required
 interface ToDo {
   id?: number;
   name?: string;
@@ -24,7 +24,32 @@ interface ToDo {
 
 const myToDo: Required<ToDo> = {
   id: 0,
-  name: "Read"
+  name: "Read",
 };
 
-// Records<Keys, Types> -> para la creacion de Objects
+// Records<Keys, Types> -> create Objects
+interface CatInfo {
+  age: number;
+  name: string;
+}
+
+type CatName = "moi" | "baris" | "luna";
+
+const cats: Record<CatName, CatInfo> = {
+  moi: { age: 1, name: "moi" },
+  baris: { age: 2, name: "baris" },
+  luna: { age: 3, name: "luna" },
+};
+
+// Pick <T, Properties> -> takes properties of an interface
+interface ToDos {
+  title: string;
+  description: string;
+  completed: boolean;
+}
+
+type ToDosPreview = Pick<ToDos, "title" | "completed">;
+const otherToDos : ToDosPreview = {
+  title: "Clean room",
+  completed: false
+}
