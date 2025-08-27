feb3d3fb425015784e7549854857d68d2d39f978
eugenia1984 <costamariaeugenia1@gmail.com>
2025-08-27 11:50:07 +0200

    feat: mapped types

--- ts_codigo_facilito/advanced/mappedTypes.ts
+++ ts_codigo_facilito/advanced/mappedTypes.ts
@@ -5,7 +5,23 @@ interface Person {
   age: number;
 }
 
-interface getPerson {
-  getName: () => string;
-  getAge: () => number;
+// Si tuviera una interface deberia hacer por cada property de la interface Person una property de GetPerson
+// interface GetPerson {
+//  getName: () => string;
+//  getAge: () => number;
+// }
+
+type Getter<T> = {
+  [Property in keyof T as `get${Capitalize<string & Property>}`]: () => T[Property];
 }
+
+type GetPerson = Getter<Person>;
+
+interface Animal {
+  id: string;
+  name: string;
+  type: number;
+  isAdopted: boolean;
+}
+
+type GetAnimal = Getter<Animal>;
