23cdcba415c26ca22026951fa6dbad2e63408d34
eugenia1984 <costamariaeugenia1@gmail.com>
2025-08-29 15:15:52 +0200

    feat: bacis types

--- ts_jap_software/Ejemplo2.js
+++ ts_jap_software/Ejemplo2.js
@@ -5,3 +5,5 @@ var soyBooleano = true;
 console.log("numero: ".concat(numero));
 console.log("cadena: ".concat(cadena));
 console.log("soyBooleano: ".concat(soyBooleano));
+var Pruebas = function (num1, num2) { return num1 - num2; };
+console.log("Pruebas(2,1): ".concat(Pruebas(2, 1)));
