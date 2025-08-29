dfdfd2aca299ec59800dec283ebd12158262da80
eugenia1984 <costamariaeugenia1@gmail.com>
2025-08-29 15:11:41 +0200

    feat: add basic types

---
+++ ts_jap_software/Ejemplo2.js
@@ -0,0 +1,7 @@
+// TS basic types
+var numero = 5;
+var cadena = "Hola";
+var soyBooleano = true;
+console.log("numero: ".concat(numero));
+console.log("cadena: ".concat(cadena));
+console.log("soyBooleano: ".concat(soyBooleano));
---
+++ ts_jap_software/Ejemplo2.ts
@@ -0,0 +1,8 @@
+// TS basic types
+let numero: Number = 5;
+let cadena: String = "Hola";
+let soyBooleano:Boolean = true;
+
+console.log(`numero: ${numero}`);
+console.log(`cadena: ${cadena}`);
+console.log(`soyBooleano: ${soyBooleano}`);
