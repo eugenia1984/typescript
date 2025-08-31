1bfe2f9f29dfeabc0bd4013937b5778f8a76cbdd
eugenia1984 <costamariaeugenia1@gmail.com>
2025-08-31 16:37:32 +0200

    feat: any example

--- ts_jap_software/Ejemplo2.js
+++ ts_jap_software/Ejemplo2.js
@@ -1,4 +1,4 @@
-// TS basic types
+// TS basic types: number, string,boolean, null, undefiend, anyno es recomendable usarla, se pierde el tipado()
 var numero = 5;
 var cadena = "Hola";
 var soyBooleano = true;
@@ -7,3 +7,6 @@ console.log("cadena: ".concat(cadena));
 console.log("soyBooleano: ".concat(soyBooleano));
 var Pruebas = function (num1, num2) { return num1 - num2; };
 console.log("Pruebas(2,1): ".concat(Pruebas(2, 1)));
+// Ejemplo de any (EVITAR USARLO)
+var variasCosas = ["Gato", 3, -4.56, true, null, undefined];
+console.log("variasCosas: ".concat(variasCosas));
--- ts_jap_software/Ejemplo2.ts
+++ ts_jap_software/Ejemplo2.ts
@@ -1,4 +1,4 @@
-// TS basic types: number, string,boolean, null, undefiend
+// TS basic types: number, string,boolean, null, undefiend, anyno es recomendable usarla, se pierde el tipado()
 let numero: number = 5;
 let cadena: string = "Hola";
 let soyBooleano:boolean = true;
@@ -9,3 +9,7 @@ console.log(`soyBooleano: ${soyBooleano}`);
 
 const Pruebas = (num1:number, num2:number): number => num1 - num2;
 console.log(`Pruebas(2,1): ${Pruebas(2,1)}`);
+
+// Ejemplo de any (EVITAR USARLO)
+let variasCosas: any[] = ["Gato", 3, -4.56, true, null, undefined];
+console.log(`variasCosas: ${variasCosas}`);
