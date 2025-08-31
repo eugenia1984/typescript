266961ce75d025637b3ca1125e0a4edb4383cd10
eugenia1984 <costamariaeugenia1@gmail.com>
2025-08-31 16:44:40 +0200

    feat: add examples

--- ts_jap_software/Ejemplo7.ts
+++ ts_jap_software/Ejemplo7.ts
@@ -1,1 +1,9 @@
-// Union types
+// Union types -> se detalla el tipo de datos que puede tomar la variable
+let numeroOrBooleano: number | boolean = 1;
+console.log(`numeroOBooleano: ${numeroOrBooleano}`);
+numeroOrBooleano = true;
+console.log(`numeroOBooleano con nuevo valor: ${numeroOrBooleano}`);
+
+// Literal
+let animal: "gato";
+console.log(`animal: ${animal}`); // undefiend
