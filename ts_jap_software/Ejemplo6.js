eb7fd03ae1836043223e2b70bdf4ccb58957776a
eugenia1984 <costamariaeugenia1@gmail.com>
2025-08-31 16:33:15 +0200

    feat: tuple example

---
+++ ts_jap_software/Ejemplo6.js
@@ -0,0 +1,6 @@
+var silla = ["Oficina", "verde", 4];
+console.log("silla[0]: ".concat(silla[0]));
+console.log("silla[1]: ".concat(silla[1]));
+console.log("silla[2]: ".concat(silla[2]));
+var silla2 = ["Living", "marron", 4];
+console.log("silla2: ".concat(silla2));
