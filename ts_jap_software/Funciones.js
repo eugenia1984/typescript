a4e07b3d2fe6eacca7a13335266eb614587a4d27
eugenia1984 <costamariaeugenia1@gmail.com>
2025-09-01 16:16:06 +0200

    feat: arrow functions

--- ts_jap_software/Funciones.js
+++ ts_jap_software/Funciones.js
@@ -18,3 +18,15 @@ function fibonacci(vecesSucesion) {
 sinRetornoNiParametros("Hola");
 console.log(sumar(3, 5));
 console.log(fibonacci(15));
+// Arrow function (Funciones flechas / anonimas)
+var sumarArrowFn = function (x, y) { return x + y; };
+var fibonacciArrowFn = function (vecesSucesion) {
+    var numeros = [0, 1];
+    var i = 2;
+    while (i <= vecesSucesion) {
+        numeros[i] = sumarArrowFn(numeros[i - 2], numeros[i - 1]);
+        i++;
+    }
+    return numeros;
+};
+console.log("fibonacciArrowFn - ".concat(fibonacciArrowFn(3)));
--- ts_jap_software/Funciones.ts
+++ ts_jap_software/Funciones.ts
@@ -22,3 +22,19 @@ function fibonacci(vecesSucesion: number): number[] {
 sinRetornoNiParametros("Hola");
 console.log(sumar(3, 5));
 console.log(fibonacci(15));
+
+// Arrow function (Funciones flechas / anonimas)
+const sumarArrowFn = (x: number, y: number): number => x + y;
+
+const fibonacciArrowFn = (vecesSucesion: number): number[] => {
+  let numeros = [0, 1];
+  let i = 2;
+
+  while (i <= vecesSucesion) {
+    numeros[i] = sumarArrowFn(numeros[i - 2], numeros[i - 1]);
+    i++;
+  }
+  return numeros;
+};
+
+console.log(`fibonacciArrowFn - ${fibonacciArrowFn(3)}`);
