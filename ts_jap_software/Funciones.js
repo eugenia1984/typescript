54e0b1a8bf7855613dd8ee74fce32c41a955f980
eugenia1984 <costamariaeugenia1@gmail.com>
2025-09-01 16:12:23 +0200

    feat: add function examples

---
+++ ts_jap_software/Funciones.js
@@ -0,0 +1,20 @@
+// Funcion sin retorno (void)
+function sinRetornoNiParametros(x) {
+    console.log(x);
+}
+function sumar(x, y) {
+    return x + y;
+}
+// Funcion que dentro invoca a otra funcion
+function fibonacci(vecesSucesion) {
+    var numeros = [0, 1];
+    var i = 2;
+    while (i <= vecesSucesion) {
+        numeros[i] = sumar(numeros[i - 2], numeros[i - 1]);
+        i++;
+    }
+    return numeros;
+}
+sinRetornoNiParametros("Hola");
+console.log(sumar(3, 5));
+console.log(fibonacci(15));
---
+++ ts_jap_software/Funciones.ts
@@ -0,0 +1,24 @@
+// Funcion sin retorno (void)
+function sinRetornoNiParametros(x: string): void {
+  console.log(x);
+}
+
+function sumar(x: number, y: number): number {
+  return x + y;
+}
+
+// Funcion que dentro invoca a otra funcion
+function fibonacci(vecesSucesion: number): number[] {
+  let numeros = [0, 1];
+  let i = 2;
+
+  while (i <= vecesSucesion) {
+    numeros[i] = sumar(numeros[i - 2], numeros[i - 1]);
+    i++;
+  }
+  return numeros;
+}
+
+sinRetornoNiParametros("Hola");
+console.log(sumar(3, 5));
+console.log(fibonacci(15));
