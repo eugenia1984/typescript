6065de76ee388602f98215e90be7d46d958a96f8
eugenia1984 <costamariaeugenia1@gmail.com>
2025-09-04 09:15:41 +0200

    feat: add class example

--- ts_jap_software/PublicPrivate.js
+++ ts_jap_software/PublicPrivate.js
@@ -1,9 +1,17 @@
-// por defecto es public, por eso no se  suele especificar-> public texto: string;
+// Por defecto es PUBLIC, por eso no se  suele especificar (accesible dentro y fuera de la clase)
+// Sino -> public texto: string;
+// PRIVATE -> no es accesibl fuera del ambito de la clase
+// Se hacen calculos, operaciones internas
 var MyObject = /** @class */ (function () {
     function MyObject(s) {
-        this.texto = s;
+        this.texto2 = s;
     }
+    MyObject.prototype.datos = function () {
+        console.log(this.texto2);
+    };
     return MyObject;
 }());
 var myObject = new MyObject("Testing");
-console.log(myObject.texto);
+console.log(myObject.datos());
+var myObject2 = new MyObject("Testing 2");
+console.log(myObject2.datos());
--- ts_jap_software/PublicPrivate.ts
+++ ts_jap_software/PublicPrivate.ts
@@ -1,11 +1,24 @@
-// por defecto es public, por eso no se  suele especificar-> public texto: string;
-class MyObject {
-  texto: string; 
+// Por defecto es PUBLIC, por eso no se  suele especificar (accesible dentro y fuera de la clase)
+// Sino -> public texto: string;
 
-  constructor(s:string) {
-    this.texto = s;
+// PRIVATE -> no es accesibl fuera del ambito de la clase
+// Se hacen calculos, operaciones internas
+
+class MyObject {
+  // private texto: string;
+  texto2: string
+
+  constructor(s: string) {
+    this.texto2 = s;
+  }
+
+  public datos() {
+    console.log(this.texto2);
   }
 }
 
-let myObject = new MyObject( "Testing");
-console.log(myObject.texto)
+let myObject = new MyObject("Testing");
+console.log(myObject.datos());
+
+let myObject2 = new MyObject("Testing 2" );
+console.log(myObject2.datos());
